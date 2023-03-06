d73f21a52ac4a68eb881e7d229702f130d1e4d50
user <malisusanth2@gmail.com>
2023-03-06 21:53:24 +0530

    Under Development

---
+++ app/src/main/java/com/internshala/kick_offassignmentsolution/FoodMap.kt
@@ -0,0 +1,11 @@
+package com.internshala.kick_offassignmentsolution
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_map)
+    }
+}
